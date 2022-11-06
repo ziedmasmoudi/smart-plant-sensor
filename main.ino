8f95e2fae9187407de876d111e4e1ba8494c6bf3
ziedmasmoudi <zied.masmoudi2001@gmail.com>
2022-11-05 21:30:24 -0400

    Created main program structure.

---
+++ main.ino
@@ -0,0 +1,15 @@
+#include "temperature.h"
+#include "humidity.h"
+#include "moisture.h"
+#include "light.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
