1f3615146734b708cf8ae76e25959c35a695eefd
ziedmasmoudi <zied.masmoudi2001@gmail.com>
2023-02-02 00:31:55 -0500

    implemented unit testing framework and comms unit test

---
+++ arduino/tests/tests.cpp
@@ -0,0 +1,14 @@
+#ifdef TEST
+
+#include "commstest.cpp"
+COMMSTEST commstest;
+void setup() {
+  // put your setup code here, to run once:
+  commstest.setup();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  commstest.loop();
+}
+#endif
