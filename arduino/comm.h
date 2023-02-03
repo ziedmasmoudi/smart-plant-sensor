18f3a2bee5f94d1af934121537352247fdb8d0a9
ziedmasmoudi <zied.masmoudi2001@gmail.com>
2023-02-02 20:41:31 -0500

    expanded comms class to allow multiple characteristics added humidity characteristic

--- arduino/comm.h
+++ arduino/comm.h
@@ -1,16 +1,27 @@
 #include <ArduinoBLE.h>
 
+enum charId {temp, humidity, count};
+
 class COMMS {
+    
+
+    BLEIntCharacteristic characteristics[2] = { BLEIntCharacteristic("2A57", BLERead | BLEWrite),
+                                              BLEIntCharacteristic("2A58", BLERead | BLEWrite)};
+      
+    
+    BLEDevice central;
 
   public:
-    BLEIntCharacteristic switchCharacteristic{"2A57", BLERead | BLEWrite};
-    BLEDevice central;
     
     void create(char name[10]);
 
     void connect();
 
-    void sendMsg(uint32_t msg);
+    void sendMsg(charId characteristic, uint32_t msg);
 
-    void readMsg(uint32_t& msg);
+    void readMsg(charId characteristic, uint32_t& msg);
+
+    bool isConnected(){
+      return central.connected();
+    }
 };
