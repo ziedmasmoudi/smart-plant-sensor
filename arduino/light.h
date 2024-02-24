93fe546cce441035724180434ca780daba59dce5
ziedmasmoudi <zied.masmoudi2001@gmail.com>
2024-02-24 00:32:31 -0500

    setup ESP32 hello world example

--- arduino/light.h
+++
@@ -1,7 +0,0 @@
-class LIGHT {
-    int ldrPin{0};
-    
-    public:
-        bool create(int ldrPin);
-        int getIntensity();
-};
--- arduino/moisture.h
+++
@@ -1,9 +0,0 @@
-class MOISTURE {
-  int powerPin {0};
-  int signalPin {0};
-  
-  public:
-      bool create(int powerPIN, int signalPIN);
-
-      int getValue();
-};
