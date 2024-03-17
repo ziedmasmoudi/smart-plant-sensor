116802b710bc79e21b7b93e0b95ee3c07a97c5f3
ziedmasmoudi <zied.masmoudi2001@gmail.com>
2024-03-17 18:28:33 -0400

    added code from esp-idf heart rate sensor NimBLE example

--- main/main.c
+++ main/main.c
@@ -6,15 +6,22 @@
 
 #include <stdio.h>
 #include <inttypes.h>
+
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "nvs_flash.h"
+#include "esp_log.h"
 
 #include "sensors_task.h"
+
+#include "bluetooth.h"
 
 static const char *TAG = __FILE__;
 
 void app_main(void)
 {
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(bluetooth_init());
     xTaskCreate(&sensors_task, "sensors_task", 2048, NULL, 5, NULL);
 }
